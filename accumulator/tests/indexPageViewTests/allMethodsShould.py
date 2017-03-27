6f661d2db87ae10b731a9353cab2952559264074
Laurence2013 <djroa1985@gmail.com>
2017-03-27 20:33:29 +0100

    Accumulator update 9

--- accumulator/tests/indexPageViewTests/allMethodsShould.py
+++ accumulator/tests/indexPageViewTests/allMethodsShould.py
@@ -6,17 +6,13 @@ from decimal import Decimal
 class AllMethodsShould(TestCase, IndexPageGamesView):
     def setUp(self):
         Game.objects.create(id=1, games='Fiorentina vs Torino', time='19:45:00', date_of_game='2017-02-27')
-
         Game.objects.create(id=2, games='Arouca vs Belenenses', time='19:45:00', date_of_game='2017-02-27')
-
         Game.objects.create(id=3, games='St Pauli vs Karlsruhe', time='19:45:00', date_of_game='2017-02-27')
 
         Odd.objects.create(id=1, home_odds=0.91, draw_odds=2.75, away_odds=3.00,
         games=Game.objects.get(pk=1))
-
         Odd.objects.create(id=2, home_odds=1.30, draw_odds=2.00, away_odds=2.20,
         games=Game.objects.get(pk=2))
-
         Odd.objects.create(id=3, home_odds=1.05, draw_odds=2.10, away_odds=2.75,
         games=Game.objects.get(pk=3))
 
