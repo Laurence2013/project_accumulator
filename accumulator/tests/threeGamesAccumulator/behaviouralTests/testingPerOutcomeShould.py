56438e0d8cfab54789457c9e47bbf6a64df32393
Laurence2013 <djroa1985@gmail.com>
2017-04-04 12:03:12 +0100

    Accumulator update 26

---
+++ accumulator/tests/threeGamesAccumulator/behaviouralTests/testingPerOutcomeShould.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from accumulator.combinations.threeGamesAccumulator import ThreeGamesAccumulator
+
+''' This is the 1st behavioural test '''
+
+class TestingPerOutcomeShould(TestCase, ThreeGamesAccumulator):
+    def setUp(self):
+        self.games = {
+            'myGame1':{'id': 1, 'game': 'Fiorentina vs Torino'},
+            'myGame2':{'id': 2, 'game': 'Arouca vs Belenenses'},
+            'myGame3':{'id': 3, 'game': 'St Pauli vs Karlsruhe'},
+        }
+        self.get_combo = self.combinationsForThreeGames(len(self.games))
+        self.combo = self.get_per_outcome(self.get_combo)
+
+    def test_LoopIndex_0_AndCompareThatTupleIs_1_And_Tuple1Is_H(self):
+        self.assertEqual([int(1),str('H')], self.combo[0])
+
+    def test_LoopIndex_1_AndCompareThatTupleIs_1_And_Tuple1Is_H(self):
+        self.assertEqual([int(1),str('H')], self.combo[1])
+
+    def test_LoopIndex_2_AndCompareThatTupleIs_1_And_Tuple1Is_H(self):
+        self.assertEqual([int(1),str('H')], self.combo[2])
+
+    def test_LoopIndex_3_AndCompareThatTupleIs_2_And_Tuple2Is_H(self):
+        self.assertEqual([int(2),str('H')], self.combo[3])
+
+    def test_LoopIndex_4_AndCompareThatTupleIs_2_And_Tuple2Is_H(self):
+        self.assertEqual([int(2),str('H')], self.combo[4])
+
+    def test_LoopIndex_5_AndCompareThatTupleIs_2_And_Tuple2Is_H(self):
+        self.assertEqual([int(2),str('D')], self.combo[5])
--- accumulator/tests/twoGamesAccumulator/behaviouralTests/twoMatchCombinationChecks.py
+++ accumulator/tests/twoGamesAccumulator/behaviouralTests/twoMatchCombinationChecks.py
@@ -12,26 +12,26 @@ class TwoMatchCombinationsChecks(TestCase, TwoGamesAccumulator):
         self.get_combo = self.combinationsForTwoGames(len(self.games))
         self.combo = self.getPerOutcome(self.get_combo)
 
-    def test_LoopIndex_0_AndCompareThatTuple0Is_1_And_Tuple1Is_H(self):
+    def test_LoopIndex_0_AndCompareThatTupleIs_1_And_Tuple1Is_H(self):
         self.assertEqual([int(1),str('H')], self.combo[0])
 
-    def test_LoopIndex_1_AndCompareThatTuple0Is_1_And_Tuple1Is_H(self):
+    def test_LoopIndex_1_AndCompareThatTupleIs_1_And_Tuple1Is_H(self):
         self.assertEqual([int(1),str('H')], self.combo[1])
 
-    def test_LoopIndex_2_AndCompareThatTuple0Is_2_And_Tuple1Is_H(self):
+    def test_LoopIndex_2_AndCompareThatTupleIs_2_And_Tuple2Is_H(self):
         self.assertEqual([int(2),str('H')], self.combo[2])
 
-    def test_LoopIndex_3_AndCompareThatTuple0Is_2_And_Tuple1Is_D(self):
+    def test_LoopIndex_3_AndCompareThatTupleIs_2_And_Tuple2Is_D(self):
         self.assertEqual([int(2),str('D')], self.combo[3])
 
-    def test_LoopIndex_4_AndCompareThatTuple0Is_3_And_Tuple1Is_H(self):
+    def test_LoopIndex_4_AndCompareThatTupleIs_3_And_Tuple3Is_H(self):
         self.assertEqual([int(3),str('H')], self.combo[4])
 
-    def test_LoopIndex_5_AndCompareThatTuple0Is_3_And_Tuple1Is_A(self):
+    def test_LoopIndex_5_AndCompareThatTupleIs_3_And_Tuple3Is_A(self):
         self.assertEqual([int(3),str('A')], self.combo[5])
 
-    def test_LoopIndex_6_AndCompareThatTuple0Is_4_And_Tuple1Is_D(self):
+    def test_LoopIndex_6_AndCompareThatTupleIs_4_And_Tuple4Is_D(self):
         self.assertEqual([int(4),str('D')], self.combo[6])
 
-    def test_LoopIndex_7_AndCompareThatTuple0Is_4_And_Tuple1Is_H(self):
+    def test_LoopIndex_7_AndCompareThatTupleIs_4_And_Tuple4Is_H(self):
         self.assertEqual([int(4),str('H')], self.combo[7])
