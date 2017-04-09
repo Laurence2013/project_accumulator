e34991edcc4968ea04f41a5210865ce650638ec6
Laurence2013 <djroa1985@gmail.com>
2017-04-09 16:03:30 +0100

    9-4-17-a

--- accumulator/combinations/threeGamesAccumulator.py
+++ accumulator/combinations/threeGamesAccumulator.py
@@ -8,6 +8,7 @@ class ThreeGamesAccumulator():
             odds1 = oddsList[c][0]
             odds2 = oddsList[c][1]
             odds3 = oddsList[c][2]
-            calc = (stake * (odds1 + 1) * (odds2 + 1) * (odds3 + 1)) - stake
+            #calc = (stake * (odds1 + 1) * (odds2 + 1) * (odds3 + 1)) - stake
+            calc = (stake * odds1 * odds2 * odds3) - stake
             calculate.append(round(calc,2))
         return calculate
--- accumulator/combinations/twoGamesAccumulator.py
+++ accumulator/combinations/twoGamesAccumulator.py
@@ -7,6 +7,7 @@ class TwoGamesAccumulator():
         for c in range(0, len(oddsList)):
             odds1 = oddsList[c][0]
             odds2 = oddsList[c][1]
-            calc = (stake * (odds1 + 1) * (odds2 + 1)) - stake
+            #calc = (stake * (odds1 + 1) * (odds2 + 1)) - stake
+            calc = (stake * odds1 * odds2) - stake
             calculate.append(round(calc,2))
         return calculate
