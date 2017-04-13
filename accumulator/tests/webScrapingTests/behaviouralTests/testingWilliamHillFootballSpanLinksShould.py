73835130184b8fd1c45f3fcbc733569a04c99540
Laurence2013 <djroa1985@gmail.com>
2017-04-13 16:34:14 +0100

    Accumulator update 13-4-17-a

--- accumulator/tests/webScrapingTests/behaviouralTests/testingWilliamHillFootballSpanLinksShould.py
+++ accumulator/tests/webScrapingTests/behaviouralTests/testingWilliamHillFootballSpanLinksShould.py
@@ -17,25 +17,21 @@ class TestingWilliamHillFootballSpanLinksShould(TestCase, ScrapingWilliamHill):
         self.get_links_0 = 'http://sports.williamhill.com/bet/en-gb/betting/y/5/tm/1/Football.html'
         self.get_links_1 = 'http://sports.williamhill.com/bet/en-gb/betting/y/5/tm/2/Football.html'
 
-    def test_sampleTest(self):
-        self.get_span_ids(self.get_links_0, 'ip_mkt_grp_tbl_292_9d8a08d4b13c912153e27659829a27ad')
-        print(ScrapingWilliamHill.span_id_lists)
+    def test_Save_SpanIdsLink_0_IntoFileAndCheckThat_SpanIdsLink_0_IsGreaterThan_0(self):
+        self.get_span_ids(self.get_links_0, self.tbody_ids_link_0)
+        self.assertGreater(len(ScrapingWilliamHill.span_id_lists), 0)
 
-    # def test_Save_SpanIdsLink_0_IntoFileAndCheckThat_SpanIdsLink_0_IsGreaterThan_0(self):
-    #     self.open_csv_file(self.get_links_0, self.tbody_ids_link_0)
-    #     self.assertGreater(len(ScrapingWilliamHill.span_id_lists), 0)
-    #
-    # def test_SaveSpanIdsLink_0_IntoCsvFile(self):
-    #     self.open_csv_file(self.get_links_0, self.tbody_ids_link_0)
-    #     self.save_file(self.span_ids_link_0, ScrapingWilliamHill.span_id_lists)
-    #     self.assertGreater(os.path.getsize(self.span_ids_link_0), 0)
-    #     self.clear_list()
-    #
-    # def test_Save_SpanIdsLink_1_IntoFileAndCheckThat_SpanIdsLink_1_IsGreaterThan_0(self):
-    #     self.open_csv_file(self.get_links_1, self.tbody_ids_link_1)
-    #     self.assertGreater(len(ScrapingWilliamHill.span_id_lists), 0)
-    #
-    # def test_SaveSpanIdsLink_1_IntoCsvFile(self):
-    #     self.open_csv_file(self.get_links_1, self.tbody_ids_link_1)
-    #     self.save_file(self.span_ids_link_1, ScrapingWilliamHill.span_id_lists)
-    #     self.assertGreater(os.path.getsize(self.span_ids_link_1), 0)
+    def test_SaveSpanIdsLink_0_IntoCsvFile(self):
+        self.get_span_ids(self.get_links_0, self.tbody_ids_link_0)
+        self.save_file(self.span_ids_link_0, ScrapingWilliamHill.span_id_lists)
+        self.assertGreater(os.path.getsize(self.span_ids_link_0), 0)
+        self.clear_list()
+
+    def test_Save_SpanIdsLink_1_IntoFileAndCheckThat_SpanIdsLink_1_IsGreaterThan_0(self):
+        self.get_span_ids(self.get_links_1, self.tbody_ids_link_1)
+        self.assertGreater(len(ScrapingWilliamHill.span_id_lists), 0)
+
+    def test_SaveSpanIdsLink_1_IntoCsvFile(self):
+        self.get_span_ids(self.get_links_1, self.tbody_ids_link_1)
+        self.save_file(self.span_ids_link_1, ScrapingWilliamHill.span_id_lists)
+        self.assertGreater(os.path.getsize(self.span_ids_link_1), 0)
--- accumulator/webScraping/scrapingWilliamHill.py
+++ accumulator/webScraping/scrapingWilliamHill.py
@@ -30,26 +30,25 @@ class ScrapingWilliamHill(GeneralGamesAccumulator):
             return None
         return tbody_ids
 
-    def get_span_ids(self, url, tag_name):
+    def get_span_ids(self, url, span_file_name):
         try:
+            csv_file = open(span_file_name)
+            csv_open = csv.reader(csv_file)
             html = urlopen(url)
             soup = BeautifulSoup(html.read(), "html5lib")
-            # games = soup.find('tbody',{'id':tag_name}).findAll('td')[2].findAll('span')
-            # games = soup.find('tbody',{'id':tag_name}).findAll('tr')
-            # games = soup.find('tbody',{'id':tag_name}).findAll('a')
-            games = soup.find('tbody',{'id':tag_name}).findAll('span',{'id':re.compile('^[0-9]')})
-            for game in games:
-                if 'id' in game.attrs:
-                    print(game.get_text().strip())
-            # for game in games:
-            #     if 'id' in game.attrs:
-            #         self.span_id_lists.append(game.attrs['id'])
+            for row in csv_open:
+                games = soup.find('tbody',{'id':row}).findAll('span',{'id':re.compile('^[0-9]')})
+                for game in games:
+                    if 'id' in game.attrs:
+                        self.span_id_lists.append(game.get_text().strip())
         except Exception as e:
             print('ExceptionError' + str(e))
             return None
         except AttributeError as e:
             print('AttributeError' + str(e))
             return None
+        finally:
+            csv_file.close()
 
     def get_all_odds_for_match(self, url, tbody_link):
         match_odds = []
@@ -72,47 +71,52 @@ class ScrapingWilliamHill(GeneralGamesAccumulator):
             csv_file.close()
         return new_match_odds
 
-    def get_name_of_teams(self, span_id_link, span_id_name):
-        team_names = []
-        try:
-            html = urlopen(span_id_link)
-            csv_file = open(span_id_name)
-            csv_open = csv.reader(csv_file)
-            soup = BeautifulSoup(html.read(), "html5lib")
-            for span_tag_name in csv_open:
-                team_names.append(soup.find('span',{'id':span_tag_name}).get_text())
-        except Exception as e:
-            print('ExceptionError' + str(e))
-            return None
-        except AttributeError as e:
-            print('AttributeError' + str(e))
-            return None
-        except TypeError as e:
-            print('TypeError' + str(e))
-            return None
-        finally:
-            csv_file.close()
-        return team_names
+    # def get_all_odds_for_match_copy(self, url, tbody_link):
+    #     match_odds = []
+    #     try:
+    #         html = urlopen(url)
+    #         csv_file = open(tbody_link)
+    #         csv_open = csv.reader(csv_file)
+    #         soup = BeautifulSoup(html.read(), "html5lib")
+    #         for odds in csv_open:
+    #             for tbody in soup.find('tbody',{'id':odds}).findAll('div',{'class':'eventprice'}):
+    #                 match_odds.append(tbody.get_text().strip())
+    #         new_match_odds = list(self.break_list_into_equal_chunks(match_odds, 3))
+    #     except Exception as e:
+    #         print('ExceptionError' + str(e))
+    #         return None
+    #     except AttributeError as e:
+    #         print('AttributeError' + str(e))
+    #         return None
+    #     finally:
+    #         csv_file.close()
+    #     return new_match_odds
+
+    # def get_name_of_teams(self, span_id_link, span_id_name):
+    #     team_names = []
+    #     try:
+    #         html = urlopen(span_id_link)
+    #         csv_file = open(span_id_name)
+    #         csv_open = csv.reader(csv_file)
+    #         soup = BeautifulSoup(html.read(), "html5lib")
+    #         for span_tag_name in csv_open:
+    #             team_names.append(soup.find('span',{'id':span_tag_name}).get_text())
+    #     except Exception as e:
+    #         print('ExceptionError' + str(e))
+    #         return None
+    #     except AttributeError as e:
+    #         print('AttributeError' + str(e))
+    #         return None
+    #     except TypeError as e:
+    #         print('TypeError' + str(e))
+    #         return None
+    #     finally:
+    #         csv_file.close()
+    #     return team_names
 
     def clear_list(self):
         self.span_id_lists.clear()
 
-    def open_csv_file(self, url, path_name):
-        csv_file = open(path_name)
-        csv_open = csv.reader(csv_file)
-        for row in csv_open:
-            try:
-                self.get_span_ids(url, row)
-            except Exception as e:
-                print('ExceptionError' + str(e))
-                return None
-            except AttributeError as e:
-                print('AttributeError' + str(e))
-                return None
-            except TypeError as e:
-                print('TypeError' + str(e))
-                return None
-        csv_file.close()
 
     def save_file(self, path_name, docs):
         csvFile = open(path_name, 'w+')
@@ -131,3 +135,20 @@ class ScrapingWilliamHill(GeneralGamesAccumulator):
             return None
         finally:
             csvFile.close()
+
+    # def open_csv_file(self, url, path_name):
+    #     csv_file = open(path_name)
+    #     csv_open = csv.reader(csv_file)
+    #     for row in csv_open:
+    #         try:
+    #             self.get_span_ids(url, row)
+    #         except Exception as e:
+    #             print('ExceptionError' + str(e))
+    #             return None
+    #         except AttributeError as e:
+    #             print('AttributeError' + str(e))
+    #             return None
+    #         except TypeError as e:
+    #             print('TypeError' + str(e))
+    #             return None
+    #     csv_file.close()
