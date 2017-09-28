3525d715d7b5fb5257c5b00c10d7506dc5994609
Laurence2013 <djroa1985@gmail.com>
2017-09-28 15:00:16 +0100

    28-9-17 e

--- games_odds/tests/webScrapingTests/behaviouralTests/williamHillGame1/testingWilliamHillDeleteTwoCsvFile2.py
+++ games_odds/tests/webScrapingTests/behaviouralTests/williamHillGame1/testingWilliamHillDeleteTwoCsvFile2.py
@@ -18,21 +18,31 @@ class TestingWilliamHillDeleteTwoCsvFile2(TestCase, MainViewsApi, WilliamHillBas
         self.factory = RequestFactory()
         self.base_dir = settings.BASE_DIR
 
-    def test_empty_2a_file_is_true(self):
+    def test_01_empty_2a_file_is_true(self):
         my_file = list()
         get_match_odds_link_1 = self.base_dir + '/games_odds/williamHillFiles/get_match_odds/get_match_odds_link_1.csv'
         my_file.append(get_match_odds_link_1)
         is_empty_file = self.empty_files(my_file)
         self.assertTrue(is_empty_file)
 
-    def test_empty_2b_file_is_true(self):
+    def test_02_empty_2b_file_is_true(self):
         my_file = list()
         get_match_odds_link_1 = self.base_dir + '/games_odds/williamHillFiles/tag_name_span_attr_ids/ids_for_tag_span_link_1.csv'
         my_file.append(get_match_odds_link_1)
         is_empty_file = self.empty_files(my_file)
         self.assertTrue(is_empty_file)
 
-    def test_william_hill_view_as_200_and_refresh_no_is_1(self):
+    def test_03_william_hill_view_as_200_and_refresh_no_is_1(self):
         request = self.factory.get(reverse('william_hill_1', args=[1]))
         response = William_Hill_Games_1.as_view()(request,1)
         self.assertEqual(response.status_code, 200)
+
+    def test_04_empty_get_match_odds_link_1_file_is_false(self):
+        file_link = self.base_dir + '/games_odds/williamHillFiles/get_match_odds/get_match_odds_link_1.csv'
+        get_file_size = self.check_file_not_empty(file_link)
+        self.assertTrue(get_file_size)
+
+    def test_05_empty_ids_for_tag_span_link_1_file_is_false(self):
+        file_link = self.base_dir + '/games_odds/williamHillFiles/tag_name_span_attr_ids/ids_for_tag_span_link_1.csv'
+        get_file_size = self.check_file_not_empty(file_link)
+        self.assertTrue(get_file_size)
