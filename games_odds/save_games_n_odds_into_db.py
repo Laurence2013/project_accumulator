b428ef9f6d0bfe363c384ddd315e2176c2677ee2
Laurence2013 <djroa1985@gmail.com>
2017-09-30 20:57:58 +0100

    30-9-17 2

--- games_odds/save_games_n_odds_into_db.py
+++ games_odds/save_games_n_odds_into_db.py
@@ -3,14 +3,13 @@ from games_odds.models import WilliamHillCsvLinks
 from games_odds.webScraping.combineOddsWithItsMatch import CombineOddsWithItsMatch
 
 class SaveGamesNOddsIntoDb(CombineOddsWithItsMatch):
-    def check_db_csv_name_with_csv_file(self, link_no):
-        base_dir = settings.BASE_DIR
-        get_url = WilliamHillCsvLinks.objects.get(url_name=link_no)
-        get_match_odds_file_path_0 = base_dir + "/games_odds/williamHillFiles/tag_name_span_attr_ids/" + get_url.ids_for_tag_span_link_csv
-        return str(get_match_odds_file_path_0)
+    def check_db_csv_name_with_csv_file(self, link_no, csv_type):
+        if link_no is str('link_0') and csv_type is str('ids_for_tag_span_link_0'):
+            get_match_odds_file_path_0 = self.get_ids_for_tag_span_link_csv(link_no)
+        return get_match_odds_file_path_0
 
-    def get_games_from_csv_file(self, link_no):
-        get_csv_file_path = self.check_db_csv_name_with_csv_file(link_no)
+    def get_games_from_csv_file(self, link_no, csv_type):
+        get_csv_file_path = self.check_db_csv_name_with_csv_file(link_no, csv_type)
         get_matches = self.get_match(get_csv_file_path)
         return get_matches
 
@@ -19,3 +18,9 @@ class SaveGamesNOddsIntoDb(CombineOddsWithItsMatch):
         for matches in get_matches:
             list_of_matches.append(matches)
         return list_of_matches
+
+    def get_ids_for_tag_span_link_csv(self, link_no):
+        base_dir = settings.BASE_DIR
+        get_url = WilliamHillCsvLinks.objects.get(url_name=link_no)
+        get_match_odds_file_path_0 = base_dir + "/games_odds/williamHillFiles/tag_name_span_attr_ids/" + get_url.ids_for_tag_span_link_csv
+        return get_match_odds_file_path_0
