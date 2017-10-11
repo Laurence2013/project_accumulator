b5878b3d0d8b511d087aa2f8fdb6c17f7f40ba7a
Laurence2013 <djroa1985@gmail.com>
2017-10-11 19:17:52 +0100

    11-10-17 2

--- games_odds/views.py
+++ games_odds/views.py
@@ -33,7 +33,6 @@ class Main_William_Hill(TemplateView, ScrapingWilliamHill):
     def get(self, request, update_no, *args, **kwargs):
         get_match_dates = []
         get_match_dates.append(str('http://sports.williamhill.com/bet/en-gb/betting/y/5/tm/Football.html'))
-        
         if int(update_no) is 1:
             if WilliamHillDailyMatche.objects.count() >= 1 or WilliamHillDailyMatche.objects.count() == 0:
                 WilliamHillDailyMatche.objects.all().delete()
