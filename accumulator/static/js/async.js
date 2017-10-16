6fbb3d329f9920baa71f5a854a4820140fb1858c
Laurence2013 <djroa1985@gmail.com>
2017-10-16 08:34:30 +0100

    16-10-17 1

--- accumulator/static/js/async.js
+++ accumulator/static/js/async.js
@@ -4,11 +4,16 @@ window.onload = function(){
   http.onreadystatechange = function(){
     if(http.readyState == 4 && http.status == 200){
       var dates = JSON.parse(http.responseText);
-      console.log(dates);
-      console.log(dates[0]['fields']);
-      console.log(dates[1]);
       var results = document.getElementById('results');
-      results.innerHTML = dates[0]['fields'].dates_of_games;
+      var mainHtml = '';
+
+      mainHtml += '<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">View all game dates<span class="caret"></span></button>';
+      mainHtml += '<ul class="dropdown-menu" role="menu">';
+      for (i = 0; i < dates.length; i++){
+        mainHtml += '<li><a href='+ i +'>' + dates[i]['fields'].dates_of_games + '</a></li>';
+      }
+      mainHtml += '</ul>';
+      results.innerHTML = mainHtml;
     }
   }
   http.open("GET", "daily_match_dates", true);
