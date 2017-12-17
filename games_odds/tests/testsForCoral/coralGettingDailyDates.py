15c42eb69ff8b3b0add14b219f4a4e300723c675
Laurence2013 <djroa1985@gmail.com>
2017-12-17 23:09:02 +0000

    17-12-17 3

--- games_odds/tests/testsForCoral/coralGettingDailyDates.py
+++ games_odds/tests/testsForCoral/coralGettingDailyDates.py
@@ -18,9 +18,18 @@ class CoralGettingDailyDates(TestCase):
     @patch.object(Coral_Base, 'get_website_title')
     def test_02_MockTestExample(self, mock_get_website_title):
         mock_get_website_title.return_value = 'http://sports.coral.co.uk/football'
-
         # action
         get_hello = self.coral.get_website_title(self.coralUrl)
-
         # assert
         self.assertEqual(self.coralUrl, get_hello)
+
+    @patch.object(Coral_Base, 'get_website_title')
+    def test_03_MockTestExample(self, mock_get_website_title):
+        Coral_Base.get_website_title(self.coralUrl)
+        mock_get_website_title.assert_called_with(self.coralUrl)
+
+    def test_04_MockTestExample(self):
+        self.coral.get_website_title = Mock()
+        self.coral.get_website_title.return_value = self.coralUrl
+        get_link = self.coral.get_website_title(self.coralUrl)
+        self.assertEqual(self.coralUrl, get_link)
